6c6918aabc74432c6d5411f8a283fd6267256ccf
guo <2246338405@qq.com>
2022-12-23 16:20:21 +0800

    第九次提交
    实现选择结果的布局搭建，点击商品参数实现对应结果的动态添加，点击删除按钮实现删除对应参数数据，封装价格变动函数

--- JS/index.js
+++ JS/index.js
@@ -351,6 +351,7 @@ window.onload = function(){
                 // 创建dd元素
                 var ddNode = document.createElement('dd')
                 ddNode.innerText = crumbData[i].data[j].type
+                ddNode.setAttribute('price',crumbData[i].data[j].changePrice)
 
                 // 让dl来追加dd
                 dlNode.appendChild(ddNode)
@@ -369,23 +370,144 @@ window.onload = function(){
          * 1、获取所有的dl元素，取其中一个dl元素下的所有dd先做测试
          * 2、循环所有的dd元素并且添加点击事件
          * 3、【排他思维】确定实际发生事件的目标对象设置文字颜色为红色，然后给其他所有元素颜色都重置为基础颜色（#666）
+         * 4、测试完毕之后在对应dl第一行下标的前面再嵌套一个for循环，目的是在变换下标
+         * 
+         * 
+         * 点击dd之后产生的mark标记
+         * 思路：
+         * 1、首先先创建一个可以容纳点击的dd元素的容器（数组），确定数组的起始长度
+         * 2、然后再将点击的dd元素的值按照对应下标写入到数组的元素身上
          */
 
         // 1、找第一个dl下的所有的dd元素
         var dlNode = document.querySelectorAll('#wrapper #content .contentMain #center .rightBottom .chooseWrap dl')
         // console.log(dlNode);
 
-        var ddNode = dlNode[0].querySelectorAll('#wrapper #content .contentMain #center .rightBottom .chooseWrap dl dd')
+        var arr = new Array(dlNode.length)
+
+        var choose = document.querySelector('#wrapper #content .contentMain #center .rightBottom .choose')
+
+        arr.fill(0)  //数组固定值填充
+
+        // 不能使用var，因为i会不断被覆盖，致使效果出不来，属于作用域链问题
+        for(let i = 0;i < dlNode.length;i++){
+           let ddNode = dlNode[i].querySelectorAll('#wrapper #content .contentMain #center .rightBottom .chooseWrap dl dd')
         // console.log(ddNode);
 
         // 2、遍历当前所有的dd元素
-        for(var i = 0;i < ddNode.length;i++){
-            ddNode[i].onclick = function(){
-                for(var j = 0;j < ddNode.length;j++){
-                    ddNode[j].style.color = '#666'
+        for(let j = 0;j < ddNode.length;j++){
+            ddNode[j].onclick = function(){
+
+                // 清空choose元素
+                choose.innerHTML = ''
+
+                for(let k = 0;k < ddNode.length;k++){
+                    ddNode[k].style.color = '#666'
                 }
                 this.style.color = "red"
+
+                // 点击哪一个dd元素动态的产生一个新的mark标记元素
+                arr[i] = this;
+                // console.log(arr);
+
+                changePriceBind(arr) //外部函数调用
+
+                // 遍历arr数组，将非0元素的值写入到mark标记
+                arr.forEach(function(value,index){
+                    // 只要是为真的条件，就动态的来创建mark标签
+                    if(value){
+                        // 创建div元素
+                        var markDiv = document.createElement('div')
+                        // 并且设置class属性
+                        markDiv.className = 'mark'
+                        // 设置值
+                        markDiv.innerText = value.innerText
+
+                        // 创建span元素
+                        var aNode = document.createElement('span')
+                        // 设置其值
+                        aNode.innerText = 'x'
+                        // 并且设置下标
+                        aNode.setAttribute('index',index)
+
+                        // 让div追加span
+                        markDiv.appendChild(aNode)
+
+                        // 让choose元素追加div
+                        choose.appendChild(markDiv)
+                    }
+                })
+
+                // 获取所有的span标签元素，并且循环发生点击事件
+                var aNodes = document.querySelectorAll('#wrapper #content .contentMain #center .rightBottom .choose .mark span')
+                // console.log(aNodes);
+
+                for(var n = 0;n < aNodes.length;n++){
+                    aNodes[n].onclick = function(){
+                        // 获取点击的span标签身上的index属性值
+                        var idx1 = this.getAttribute('index')
+                        // console.log(idx1);
+
+                        // 恢复数组中对应下标元素的值
+                        arr[idx1] = 0
+
+                        // 查找对应下标的那个dl行中的所有的dd元素
+                        var ddlist = dlNode[idx1].querySelectorAll('dd')
+
+                        // 遍历所有的dd元素
+                        for(var m = 0;m < ddlist.length;m++){
+                            // 其余所有的dd文字颜色为灰色
+                            ddlist[m].style.color = '#666'
+                        }
+
+                        // 默认的第一个dd文字颜色恢复成红色
+                        ddlist[0].style.color = 'red'
+
+                        // 删除对应下下标位置的mark标记,this指向的是span，它的paren为markDiv
+                        choose.removeChild(this.parentNode)
+                        // console.log(this.parentNode);
+
+                        // 调用价格函数
+                        changePriceBind(arr)
+                    }
+                }
+            }
+        } 
+        }
+        
+    }
+
+    // 价格变动的函数声明
+    /**
+     * 这个函数需要在点击dd的时候以及删除mark标记的时候才需要调用
+     */
+    function changePriceBind(arr){
+        /**
+         * 思路：
+         * 1、获取价格的标签元素
+         * 2、给每一个dd标签身上默认都设置一个自定义属性，用来记录变化的价格
+         * 3、遍历arr数组，将dd元素身上的新变化的价格与已有的价格（5299）相加
+         * 4、最后将计算之后的结果重新渲染到p标签中
+         */
+
+        // 1、获取原价格标签元素
+        var oldPrice = document.querySelector('#wrapper #content .contentMain #center .right .rightTop .priceWrap .priceTop .price')
+
+        // 2、记录
+
+        // 取出默认的价格
+        var price = goodData.goodsDetail.price
+
+        // 3、遍历arr数组
+        for(var i = 0;i < arr.length;i++){
+            if (arr[i]) {
+                // 数据类型的强制转换
+                var changePrice = Number(arr[i].getAttribute('price'));
+                //最终的价格
+                price = price + changePrice
             }
         }
+
+        oldPrice.innerText = price
     }
 }
